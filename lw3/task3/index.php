fa6653601e9c91e653c5d300477e3435f8ae7192
darzo28 <darzo28@yandex.ru>
2021-03-01 22:29:25 +0300

    add lw3/task3

---
+++ lw3/task3/index.php
@@ -0,0 +1,58 @@
+<?php
+function getGETParameter(string $name): ?string
+{
+	return isset($_GET[$name]) ? (string)$_GET[$name] : null;
+}
+
+function isPassword(string $str) : bool
+{
+	$non_word_char = preg_match("/\W|\_/", $str, $matches);
+
+	return $non_word_char ? false : true;
+}
+
+function checkPasswordStrength(string $str) : int
+{
+	$len = strlen($str);
+	foreach (count_chars($str, 1) as $i => $val) 
+	{   
+		if (($i >= 48) && ($i <= 57))
+		{
+			$digits += $val;
+		}
+		elseif (($i >= 65) && ($i <= 90))
+		{
+			$uppercase += $val;
+		}
+		elseif (($i >= 97) && ($i <= 122))
+		{
+			$lowercase += $val;
+		}
+		$repeats += ($val > 1) ? $val : 0;
+   		
+	}
+	$strength = ($uppercase) ? 2*($len - $uppercase) : 0;
+	$strength += ($lowercase) ? 2*($len - $lowercase) : 0;
+	$strength += (($digits > 0) xor (($uppercase + $lowercase) > 0)) ? -$len : 0;
+	$strength += 4*$len + 4*$digits - $repeats;
+
+	return $strength;
+}
+
+$param = "password";
+$text = getGETParameter($param);
+
+header("Content-Type: text/plain");
+
+if ($text === null)
+{
+	echo "Parameter \"$param\" isn't found";
+}
+elseif (($text === "") || !isPassword($text))
+{
+	echo "Enter valid password";
+}
+else
+{
+	echo checkPasswordStrength($text);
+}
